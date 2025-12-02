b0c7a9d14453cdbd873616644a419ea3b84dd599
Cristian Saqueta Melo <mkd_3d@hotmail.com>
2025-12-01 23:50:14 -0300

    fix(worlds): llamar a loadWorlds() en lugar de loadWorldsSection() obsoleta

--- web/static/dashboard.js
+++ web/static/dashboard.js
@@ -158,7 +158,7 @@ function showSection(sectionName) {
             loadFilesTree();
             break;
         case 'worlds':
-            loadWorldsSection();
+            loadWorlds();
             break;
         case 'backups':
             loadBackupsSection();
